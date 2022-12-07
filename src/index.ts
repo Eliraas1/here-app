b30bfead5fe18aaff0ec906c2b51d43f2b24e37a
Eliraas1 <eliraas1@ac.sce.ac.il>
2022-12-07 15:13:03 +0200

    cccc

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import express, { json } from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-// import routes from "./Routes";
-// import { errorHandling } from "./Services/error.services";
+import routes from "./Routes";
+import { errorHandling } from "./Services/error.services";
 const app = express();
 const PORT = process.env.PORT || "3000";
-// const connect = () => {
-//     mongoose
-//         .connect(process.env.MONGO_PATH as string, { dbName: "HereApp" })
-//         .then(() => {
-//             console.log("connected to mongodb");
-//         })
-//         .catch((err) => {
-//             throw err;
-//         });
-// };
-// app.use(json({ limit: "21mb" }));
-// app.use(routes);
-// app.use(errorHandling);
+const connect = () => {
+    mongoose
+        .connect(process.env.MONGO_PATH as string, { dbName: "HereApp" })
+        .then(() => {
+            console.log("connected to mongodb");
+        })
+        .catch((err) => {
+            throw err;
+        });
+};
+app.use(json({ limit: "21mb" }));
+app.use(routes);
+app.use(errorHandling);
 app.listen(PORT, () => {
-    // connect();
+    connect();
     console.log("Connected");
 });
