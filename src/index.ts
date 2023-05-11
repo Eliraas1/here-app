c8dc4f7301ecc1ea19365511c5f400f042a3af90
omeratt <omeratt1405@gmail.com>
2023-05-11 16:44:51 +0300

    new refactor

--- src/index.ts
+++ src/index.ts
@@ -61,20 +61,22 @@ const assuming: Assuming[] = XLSX.utils.sheet_to_json(assumingXL);
 const maleDeath: Death[] = XLSX.utils.sheet_to_json(maleDeathXL);
 const femaleDeath: Death[] = XLSX.utils.sheet_to_json(femaleDeathXL);
 const data = unFilterData.filter((item) => !item["תאריך עזיבה "]);
+const dateStr = "31/12/2022";
+const currentDate = moment(dateStr, "DD/MM/YYYY");
 const getAge = (item: Data) => {
-    const currentDate = moment("12-31-22");
+    // const currentDate = moment("12-31-22");
     const birthDate = moment(item["תאריך לידה"]);
     const age = currentDate.diff(birthDate, "years"); //x
     return age;
 };
 const getSeniority = (item: Data) => {
-    const currentDate = moment("12-31-22");
+    // const currentDate = moment("12-31-22");
     const startDate = moment(item["תאריך תחילת עבודה "]);
     const seniority = currentDate.diff(startDate, "years"); //x
     return seniority;
 };
 const getSectionSeniority = (item: Data) => {
-    const currentDate = moment();
+    // const currentDate = moment();
     const startDate = moment(item["תאריך  קבלת סעיף 14"]);
     const sectionSeniority = currentDate.diff(startDate, "years"); //x
     return sectionSeniority;
@@ -85,19 +87,23 @@ data.forEach((item: Data) => {
     item.index = index;
     item["W"] = item.מין === "M" ? 67 : 64;
     item["אחוז סעיף 14"] &&= item["אחוז סעיף 14"] / 100;
+
     item.age = getAge(item);
-    const date = moment(item["תאריך תחילת עבודה "]);
+    // const date = moment(item["תאריך תחילת עבודה "]);
     item.seniority = getSeniority(item);
     item.salaryGrowRate = index % 2 === 0 ? 0.04 : 0.02;
     // check when did user get section14 and calc
+    // const dada = item.seniority - getSectionSeniority(item);
     const dada = item.seniority - getSectionSeniority(item);
     const sal1 = dada * item["שכר "];
     const sal2 = !item["אחוז סעיף 14"]
         ? 0
         : getSectionSeniority(item) * item["שכר "] * (1 - item["אחוז סעיף 14"]);
+
     item.totalSalary = sal1 + sal2;
-    item.index === 1 &&
-        console.log(dada, sal1, sal2, getSectionSeniority(item), item);
+    // item.index === 1 &&
+    //     console.log(dada, sal1, sal2, getSectionSeniority(item), item);
+    // console.log(item["אחוז סעיף 14"]);
 });
 
 const leaveProbability: LeaveProbability[] = [];
@@ -130,18 +136,17 @@ const discountRate = fixedDiscountRate();
 
 const getPX = (t: number, item: Data) => {
     if (t <= 0) return 1;
-
     const _t = t + item.age;
-    return 1 - getQ1(_t, item) - getQ2(_t, item) - getQ3(_t, item);
+    return 1 - getQ1(_t) - getQ2(_t) - getQ3(_t, item);
 };
-const getQ1 = (t: number, item?: Data) => {
+const getQ1 = (t: number) => {
     const index = getIndexOfProbability(t);
     const probabilityItem = leaveProbability[index];
-    if (!probabilityItem)
-        console.log("------------------------------", { t, index });
+    // if (!probabilityItem)
+    // console.log("------------------------------", { t, index });
     return probabilityItem.פיטורין;
 };
-const getQ2 = (t: number, item?: Data) => {
+const getQ2 = (t: number) => {
     const index = getIndexOfProbability(t);
     const probabilityItem = leaveProbability[index];
     return probabilityItem.התפטרות;
@@ -149,104 +154,107 @@ const getQ2 = (t: number, item?: Data) => {
 const getQ3 = (t: number, item?: Data) => {
     if (!item) return 0;
     const death = item.מין === "M" ? maleDeath : femaleDeath;
-    const Qx = death.find((i) => i.age === t + 1)?.["q(x)"];
-    item.index === 1 && console.log(t, item.age, Qx);
+    const Qx = death.find((i) => i.age === t)?.["q(x)"];
+    // item.index === 1 && console.log(t, item.age, Qx);
     return Qx || 0;
 };
 
-const calc1 = (item: Data) => {
-    const w = item.W;
-    const x = item.age;
-
-    let sum = 0;
-    for (let t = 0; t < w - x - 2; t++) {
-        const _discountRate = discountRate[t];
-        const salaryGrowRate = t > 0 && t % 2 == 0 ? item.salaryGrowRate : 0;
-        const power = t + 0.5;
-        const part1 = item.totalSalary;
-        const part2 =
-            ((1 + salaryGrowRate) ** power *
-                getPX(t + 1, item) *
-                getQ1(t + 1 + x, item)) /
-            (1 + _discountRate) ** power;
-        sum += part1 * part2;
-    }
-    return sum;
-};
-const calc2 = (item: Data) => {
+const sigma1 = (item: Data) => {
     const w = item.W;
     const x = item.age;
     let sum = 0;
-    for (let t = 0; t < w - x - 2; t++) {
-        const power = t + 0.5;
-        const _t = t + 1 + x;
-        const salaryGrowRate = t > 0 && t % 2 == 0 ? item.salaryGrowRate : 0;
-        const part1 = item.totalSalary;
-        const part2 =
-            ((1 + salaryGrowRate) ** power *
-                getPX(_t - x, item) *
-                getQ3(_t, item)) /
-            (1 + discountRate[t]) ** power;
-        sum += part1 * part2;
+    for (let t = 0; t <= w - x - 2; t++) {
+        const lastSalary = item.totalSalary;
+        let growRate = 0;
+        if (t > 0 && t % 2 == 0) {
+            growRate = item.salaryGrowRate;
+        }
+        const numer =
+            (1 + growRate) ** (t + 0.5) *
+            (getPX(t + 1, item) * getQ1(x + t + 1));
+        const denom = (1 + discountRate[t]) ** (t + 0.5);
+        sum += (lastSalary * numer) / denom;
     }
     return sum;
 };
-const calc3 = (item: Data) => {
+const sigma2 = (item: Data) => {
     const w = item.W;
     const x = item.age;
-
+    let sum = 0;
+    for (let t = 0; t <= w - x - 2; t++) {
+        const lastSalary = item.totalSalary;
+        let growRate = 0;
+        if (t > 0 && t % 2 == 0) {
+            growRate = item.salaryGrowRate;
+        }
+        const numer =
+            (1 + growRate) ** (t + 0.5) *
+            (getPX(t + 1, item) * getQ3(x + t + 1, item));
+        const denom = (1 + discountRate[t]) ** (t + 0.5);
+        sum += (lastSalary * numer) / denom;
+    }
+    return sum;
+};
+const sigma3 = (item: Data) => {
+    const w = item.W;
+    const x = item.age;
     const assetsValue = item["שווי נכס"] || 0;
     let sum = 0;
-    for (let t = 0; t < w - x - 2; t++) {
-        const _t = t + 1 + x;
-        sum += assetsValue * getPX(_t - x, item) * getQ2(_t, item);
+    for (let t = 0; t <= w - x - 2; t++) {
+        sum += assetsValue * getPX(t + 1, item) * getQ2(x + t + 1);
     }
     return sum;
 };
 
 const calcRest = (item: Data) => {
-    const w = item.W;
-    const x = item.age;
-    const power = w - x + 0.5;
-    const power2 = power - 1;
-    const t = w - x - 1;
-    const salaryGrowRate = item.salaryGrowRate;
-    const assetsValue = item["שווי נכס"] || 0;
-    const commonPart = item.totalSalary * 3;
-    const discountRate1 = discountRate[w - x];
-    const part1 =
-        ((1 + salaryGrowRate) ** power * getPX(t, item) * getQ1(w - 1, item)) /
-        (1 + discountRate1) ** power;
-    const part2 =
-        ((1 + salaryGrowRate) ** power2 *
-            getPX(w - x - 1, item) *
-            getQ3(w - 1, item)) /
-        (1 + discountRate[w - x - 1]);
-    const separatedPart = assetsValue * getPX(t, item) * getQ2(w - 1, item);
-    const part3 =
-        ((1 + salaryGrowRate) ** (w - x) *
-            getPX(t, item) *
-            (1 - getPX(w - 1 - x, item))) /
-        (1 + discountRate1) ** (w - x);
-
-    const a = (1 + discountRate[w - x]) ** 1;
-    const b = (1 + discountRate[w - x - 1]) ** 1;
-    if (isNaN(a))
-        console.log("---- a ---", discountRate[w - x], w - x, x, item.מין);
-    if (isNaN(b)) console.log("---- b ---", b);
-
-    const sum = commonPart * (part1 + part2 + part3) + separatedPart;
-    return sum;
+    const firstLine = calcRestFirstLine(item);
+    const secondLine = calcRestSecondLine(item);
+    const thirdLine = calcRestThirdLine(item);
+    return firstLine + secondLine + thirdLine;
 };
 
+const calcRestFirstLine = (item: Data) => {
+    const numer =
+        item.totalSalary *
+        (1 + item.salaryGrowRate) ** (item.W - item.age + 0.5) *
+        getPX(item.W - item.age - 1, item) *
+        getQ1(item.W - 1);
+    const denom =
+        (1 + discountRate[item.W - item.age]) ** (item.W - item.age + 0.5);
+    return numer / denom;
+};
+const calcRestSecondLine = (item: Data) => {
+    const numer =
+        item.totalSalary *
+        (1 + item.salaryGrowRate) ** (item.W - item.age - 1 + 0.5) *
+        getPX(item.W - item.age - 1, item) *
+        getQ3(item.W - 1);
+    const denom =
+        (1 + discountRate[item.W - item.age]) ** (item.W - item.age - 1 + 0.5);
+    const assetsCalculation =
+        item["שווי נכס"]! *
+        getPX(item.W - item.age - 1, item) *
+        getQ2(item.W - 1);
+    return numer / denom + assetsCalculation;
+};
+const calcRestThirdLine = (item: Data) => {
+    const numer =
+        item.totalSalary *
+        (1 + item.salaryGrowRate) ** (item.W - item.age) *
+        getPX(item.W - item.age - 1, item) *
+        (1 - getQ1(item.W - 1) - getQ2(item.W - 1) - getQ3(item.W - 1));
+    const denom = (1 + discountRate[item.W - item.age]) ** (item.W - item.age);
+    return numer / denom;
+};
 const calcAll = (item: Data) => {
     let sum = 0;
-    if (item.age > item.W) {
-        sum = item["שכר "] * item.seniority * (1 - (item["אחוז סעיף 14"] || 1));
+    if (item.age >= item.W) {
+        console.log(item.index);
+        sum = item["שכר "] * item.seniority * (1 - (item["אחוז סעיף 14"] || 0));
         return sum;
     }
 
-    sum = calc1(item) + calc2(item) + calc3(item) + calcRest(item);
+    sum = sigma1(item) + sigma2(item) + sigma3(item) + calcRest(item);
     return sum;
 };
 
@@ -257,10 +265,12 @@ data.forEach((item, index) => {
         salary: calcAll(item).toLocaleString(),
     });
 });
-var filename = "write.xlsx";
-var ws_name = "results";
-var wb = XLSX.utils.book_new();
-var ws = XLSX.utils.json_to_sheet(table);
-XLSX.utils.book_append_sheet(wb, ws, ws_name);
-XLSX.writeFile(wb, filename);
-console.table(table);
+// var filename = "write.xlsx";
+// var ws_name = "results";
+// var wb = XLSX.utils.book_new();
+// var ws = XLSX.utils.json_to_sheet(table);
+// XLSX.utils.book_append_sheet(wb, ws, ws_name);
+// XLSX.writeFile(wb, filename);
+// console.table(table);
+console.table(assuming);
+console.log(discountRate[discountRate.length - 1]);
