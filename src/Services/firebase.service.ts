75f4ba32282168bfd58d4259b80a544b132647dc
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-06-06 18:22:23 +0300

    .

--- src/Services/firebase.service.ts
+++ src/Services/firebase.service.ts
@@ -11,33 +11,6 @@ interface INotification {
     title: string;
     body: string;
 }
-// export const sendPushNotification = async (
-//     user?: UserType,
-//     task?: TaskType
-// ) => {
-//     if (!user || !task) return;
-//     const deviceTokens = user.fcmToken;
-//     const [hour, minutes] = task.targetDate
-//         ?.toISOString()
-//         .split("T")[1]
-//         ?.split(":") || ["", ""];
-//     const fixedHours = [hour, minutes].join(":");
-//     const title = `Hey 👋 the task ${task.name} has not completed yet`;
-//     const body = `you have till ${fixedHours} ⏳`;
-//     const message = {
-//         tokens: deviceTokens,
-//         notification: {
-//             title,
-//             body,
-//         },
-//     };
-//     try {
-//         const response = await admin.messaging().sendEachForMulticast(message);
-//         console.log("Successfully sent push notification:", response);
-//     } catch (error) {
-//         console.error("Error sending push notification:", error);
-//     }
-// };
 
 export const sendNotification = async (user?: UserType, task?: TaskType) => {
     if (!user || !task) return;
